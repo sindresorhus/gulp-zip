0266b0cff18ccf1a1562f45c64eb14217b1db418
Dimonchik <dmitriy.dn.ua@gmail.com>
2017-07-22 17:05:59 +0300

    =Add functional for tree category

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ module.exports = (filename, opts) => {
 	}
 
 	opts = Object.assign({
-		compress: true
+		compress: true,
+        category_merge:true
 	}, opts);
 
-	let firstFile;
+	let firstFile, collect_category=[];
 	const zip = new Yazl.ZipFile();
 
 	return through.obj((file, enc, cb) => {
@@ -30,11 +31,34 @@ module.exports = (filename, opts) => {
 			return;
 		}
 
+		var patch_directory=file.base;
+        patch_directory=patch_directory.replace(file.cwd+'/',"");
+
 		if (file.isNull() && file.stat && file.stat.isDirectory && file.stat.isDirectory()) {
 			zip.addEmptyDirectory(pathname, {
 				mtime: file.stat.mtime || new Date(),
 				mode: file.stat.mode
 			});
+		} else if(patch_directory.length && !opts.category_merge) { // when we want save directory structure
+            const stat = {
+                compress: opts.compress,
+                mtime: file.stat ? file.stat.mtime : new Date(),
+                mode: file.stat ? file.stat.mode : null
+            };
+
+            if(collect_category.indexOf(patch_directory)==-1) {
+				zip.addEmptyDirectory(patch_directory);
+            	collect_category.push(patch_directory);
+			}
+
+            if (file.isStream()) {
+                zip.addReadStream(file.contents, patch_directory+patch_directory, stat);
+            }
+
+            if (file.isBuffer()) {
+                zip.addBuffer(file.contents, patch_directory+pathname, stat);
+            }
+
 		} else {
 			const stat = {
 				compress: opts.compress,
