677e2050b384fbc044d7ab0b12bf9da056066448
Louis Orleans <louis@orleans.io>
2018-10-26 03:01:11 -0700

    ✨ add index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,15 @@
-declare module 'gulp-zip' {
-  import { File } from 'gulp-util';
-
-  interface IGulpZipOptions {
-    /**
-     * Compress
-     * @default true
-     */
-    compress?: boolean;
-  }
-
-  const gulp_zip: (filename: string, options?: IGulpZipOptions) => NodeJS.ReadStream;
-
-  export = gulp_zip;
+interface Options {
+	/**
+	 * @default true
+	 */
+	compress?: boolean;
+	/**
+	 * Overrides the modification timestamp for all files added to the archive.
+	 *
+	 * Tip: Setting it to the same value across executions enables you to create stable archives—archives that change only when the contents of their entries change, regardless of whether those entries were "touched" or regenerated.
+	 */
+	modifiedTime?: Date;
 }
+
+/** ZIP compress files */
+export default function (filename: string, options?: Options): NodeJS.ReadStream;
